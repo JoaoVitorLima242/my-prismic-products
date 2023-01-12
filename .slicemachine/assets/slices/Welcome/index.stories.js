81676a1e8d047fa838779b3573cdc5259378c6f0
JoaoVitorLima242 <jlima0500@gmail.com>
2023-01-12 14:48:10 -0300

    add prismic to next

---
+++ .slicemachine/assets/slices/Welcome/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Welcome';
+
+export default {
+  title: 'slices/Welcome'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Slave","spans":[]}],"description":[{"type":"paragraph","text":"Est pariatur aute incididunt aute. Fugiat consequat consectetur Lorem labore deserunt laboris esse ad commodo quis sunt mollit pariatur aliquip nostrud.","spans":[]}]},"id":"_Default","slice_type":"welcome"}} />
+_Default.storyName = ''
